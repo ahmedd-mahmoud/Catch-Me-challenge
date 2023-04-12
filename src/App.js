24ec9852387cba4ac0b657d5f67a5e7845cad1cf
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2023-04-12 21:41:22 +0200

    Level 2 (initial code)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import RandomButton from "./Components/RandomButton";
-import "./App.css";
 
 function App() {
   return (
     <div>
-      <div>
-        <p id="title">Catch me if you can ","</p>
-      </div>
-      <div id="container">
-        <RandomButton />
-      </div>
+      <RandomButton />
     </div>
   );
 }
--- src/Components/RandomButton/index.js
+++ src/Components/RandomButton/index.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import "./index.css";
 
 const RandomButton = () => {
-  const [position, setPosition] = useState({ top: "49%", left: "45%" });
+  const [position, setPosition] = useState({ top: "50%", left: "50%" });
+  const [text, setText] = useState("Start");
+  const [count, setCount] = useState(0);
 
   const handleClick = () => {
     const container = document.getElementById("container");
@@ -18,18 +20,28 @@ const RandomButton = () => {
       top: randomTop,
       left: randomLeft,
     });
+
+    if (text === "Start") {
+      setText("Click me!");
+    }
+    setCount(count + 1);
   };
 
   const buttonStyle = {
-    position: "absolute",
     top: position.top,
     left: position.left,
   };
 
   return (
-    <button onClick={handleClick} id="random-button" style={buttonStyle}>
-      Click me!
-    </button>
+    <div>
+      <p id="title-text">Catch me if you can:</p>
+      <p id="counter-text">Counter: {count}</p>
+      <div id="container">
+        <button onClick={handleClick} id="random-button" style={buttonStyle}>
+          {text}
+        </button>
+      </div>
+    </div>
   );
 };
 
