9658b1c0606f063a5d17c424e3666a3f5f8cc194
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2023-04-14 07:28:53 +0200

    Level 3

--- src/Components/RandomButton/index.js
+++ src/Components/RandomButton/index.js
@@ -1,16 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./index.css";
 
-const RandomButton = ({ count, setCount }) => {
-  const [position, setPosition] = useState({ top: "50%", left: "47%" });
-  const [text, setText] = useState("Start");
+const RandomButton = ({ count, setCount, containerRef }) => {
+  const [position, setPosition] = useState({ top: "50%", left: "50%" });
+  const [text, setText] = useState("Start Game");
+  const [gameState, setGameState] = useState("initial");
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    if (gameState === "in game") {
+      const timer = setTimeout(() => {
+        setGameState("lost");
+      }, 10000);
+      return () => clearTimeout(timer);
+    }
+  }, [gameState]);
 
   const handleClick = () => {
-    const container = document.getElementById("container");
-    const button = document.getElementById("random-button");
+    if (gameState === "initial") {
+      setGameState("in game");
+      setText("Catch me!");
+    } else if (gameState === "in game") {
+      if (count === 4) {
+        setGameState("won");
+      }
+    }
+    setCount(count + 1);
 
-    const containerWidth = container.clientWidth - button.clientWidth;
-    const containerHeight = container.clientHeight - button.clientHeight;
+    const container = containerRef.current;
+    const button = buttonRef.current;
+
+    const containerWidth = container.offsetWidth - button.offsetWidth;
+    const containerHeight = container.offsetHeight - button.offsetHeight;
 
     const randomLeft = Math.random() * containerWidth;
     const randomTop = Math.random() * containerHeight;
@@ -19,11 +40,6 @@ const RandomButton = ({ count, setCount }) => {
       top: randomTop,
       left: randomLeft,
     });
-
-    if (text === "Start") {
-      setText("Click me!");
-    }
-    setCount(count + 1);
   };
 
   const buttonStyle = {
@@ -32,9 +48,30 @@ const RandomButton = ({ count, setCount }) => {
   };
 
   return (
-    <button onClick={handleClick} id="random-button" style={buttonStyle}>
-      {text}
-    </button>
+    <>
+      {gameState === "won" && (
+        <div className="result-text">
+          <h1>You won!</h1>
+          <p className="refresh-text">Refresh to try again</p>
+        </div>
+      )}
+      {gameState === "lost" && (
+        <div className="result-text">
+          <h1>Game over!</h1>
+          <p className="refresh-text">Refresh to try again</p>
+        </div>
+      )}
+      {gameState !== "won" && gameState !== "lost" && (
+        <button
+          ref={buttonRef}
+          onClick={handleClick}
+          id="random-button"
+          style={buttonStyle}
+        >
+          {text}
+        </button>
+      )}
+    </>
   );
 };
 
