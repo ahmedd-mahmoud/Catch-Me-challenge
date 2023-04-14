1ce97070d2bd45876237a12498386cf6583d8014
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2023-04-14 07:28:38 +0200

    Level 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import RandomButton from "./Components/RandomButton";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const containerRef = useRef(null);
   return (
     <div>
       <div>
         <p id="title">Catch me if you can:</p>
-        <p id="counter-text">Counter: {count}</p>
+        <p id="score-text">Score: {count}</p>
       </div>
-      <div id="container">
-        <RandomButton count={count} setCount={setCount} />
+      <div id="container" ref={containerRef}>
+        <RandomButton
+          count={count}
+          setCount={setCount}
+          containerRef={containerRef}
+        />
       </div>
     </div>
   );
